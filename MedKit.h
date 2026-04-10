ff80d25620fbd9e853f03722be81911099a6a112
MPR-911 <prakashraj7785@gmail.com>
2026-04-10 21:20:59 +0530

    Health

---
+++ MedKit.h
@@ -0,0 +1,19 @@
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "MedKit.generated.h"
+
+UCLASS()
+class UNREALCPP_API AMedKit : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMedKit();
+
+	UFUNCTION()
+	void OnOverlap(AActor* MyOverlappedActor, AActor* OtherActor);
+
+};
