1b0fdb6a0e695b905fcd4dba8568249a07f41a6c
OM-DEV-CREATIONS <prakashraj7785@gmail.com>
2026-04-16 22:30:29 +0530

    Health

---
+++ ShooterProjectile.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "ShooterProjectile.generated.h"
+
+UCLASS()
+class UNREALCPP_API AShooterProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AShooterProjectile();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+	
+};
